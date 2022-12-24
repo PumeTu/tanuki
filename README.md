7ba0edc4e626b023dd1f8f638ad10c3dfbdcee6f
PumeTu <pumetuchinda@gmail.com>
2022-12-24 12:48:00 -0500

    add skeleton codes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tinycnn
+
+Implemenetation of both a CNN in torch without utilizng the autograd or the nn libraries and performing manual foward and backward passes. This is just meant to check my understanding of CNNs and a cool fun side project.
---
+++ test/test_layers.py
@@ -0,0 +1,13 @@
+import unittest
+import torch
+
+from tinycnn.layers import Convolution, BatchNorm2d, ReLU
+
+class TestConvolution(unittest.TestCase):
+    pass
+
+class TestBatchNorm2d(unittest.TestCase):
+    pass
+
+class TestReLU(unittest.TestCase):
+    pass
